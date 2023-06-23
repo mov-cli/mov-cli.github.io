ab9fa0d3bdecfd2221e01dfc1c00e03b443f0e21
Ananas <132799819+HLOAnanas@users.noreply.github.com>
2023-06-23 17:55:47 +0200

    update website

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["about/index.html", "developers/index.html", "404.html", "index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

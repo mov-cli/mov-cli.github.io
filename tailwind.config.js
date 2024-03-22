88095d1f35f257a979ea4a7fb4b5157ddba6cc77
R3tr0Ananas <132799819+r3tr0ananas@users.noreply.github.com>
2024-03-22 14:08:45 +0100

    new year new me

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["about/index.html", "developers/index.html", "404.html", "index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-

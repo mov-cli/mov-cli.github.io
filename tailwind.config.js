28d6f5ae566df09bd4bdaa8963b22a0ab6624f88
R3tr0Ananas <132799819+r3tr0ananas@users.noreply.github.com>
2024-04-07 04:10:59 +0200

    feat: use swiper (PAIN)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        "spacegrotesk": ["Space Grotesk"]
-      },
-      colors: {
-        aGrey: {
-          DEFAULT: "#505050",
-          100: "#606060",
-          300: "#505050",
-          500: "#404040",
-          700: "#2d2d2d",
-          800: "#282828"
+    content: ["./src/**/*.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+          "spacegrotesk": ["Space Grotesk"]
         },
-        aDark: {
-          DEFAULT: "#0e1114",
-          100: "#2e3741",
-          500: "#191e23",
-          800: "#0e1114"
-        }
+        colors: {
+          aGrey: {
+            DEFAULT: "#505050",
+            100: "#606060",
+            300: "#505050",
+            500: "#404040",
+            700: "#2d2d2d",
+            800: "#282828"
+          },
+          aDark: {
+            DEFAULT: "#0e1114",
+            100: "#2e3741",
+            500: "#191e23",
+            800: "#0e1114"
+          }
+        },
       },
-    },
+    }
   }
-}
